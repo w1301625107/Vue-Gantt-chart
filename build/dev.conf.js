fb5f71aab28ae9449025415fe082af5e90a6b40d
imba <>
2018-09-29 22:35:08 +0800

    去除无用代码

--- build/dev.conf.js
+++ build/dev.conf.js
@@ -53,17 +53,7 @@ module.exports = {
     overlay: true,
     //publicPath: assetsPath(),
     //quiet: true, // necessary for FriendlyErrorsPlugin
-    proxy: {
-      '/api': {
-
-        target: 'https://douban.uieee.com/v2/', //设置你调用的接口域名和端口号 别忘了加http
-        //target: 'http://api.douban.com/v2', //设置你调用的接口域名和端口号 别忘了加http
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '' //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
-        }
-      }
-    },
+    
   },
 
   // 加载器
