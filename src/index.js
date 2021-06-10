e6ebfe17077a18adec97180a0c077e5e2f5743ba
w1301625107 <1301625107@qq.com>
2021-06-10 09:14:57 +0800

    doc: 修复文档中的错误的默认值 feat: 修改导出的内容，支持页面单独引入 doc: 修改packjson中main的值

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import gantt from "./gantt.vue";
 
 // 定义我们的插件
-const myPlugin = {
-  // eslint-disable-next-line
-  version:__VERSION__,
-  // 该插件有一个install方法
-  // 方法的第一个参数是传入的Vue，第二个参数可以插件的自定义参数
-  // eslint-disable-next-line
-  install(Vue, options) {
-    // 将其注册为vue的组件，'gantt'是组件名
-    Vue.component("v-gantt-chart", gantt);
-  }
+// const myPlugin = {
+//   // eslint-disable-next-line
+//   version:__VERSION__,
+//   // 该插件有一个install方法
+//   // 方法的第一个参数是传入的Vue，第二个参数可以插件的自定义参数
+//   // eslint-disable-next-line
+//   install(Vue, options) {
+//     // 将其注册为vue的组件，'gantt'是组件名
+//     Vue.component("v-gantt-chart", gantt);
+//   }
+// };
+
+gantt.version = "__VERSION__";
+gantt.install = function(Vue) {
+  //     // 将其注册为vue的组件，'gantt'是组件名
+  Vue.component("v-gantt-chart", gantt);
 };
 // 新增
 if (typeof window !== "undefined" && window.Vue) {
-  window.Vue.use(myPlugin);
+  window.Vue.use(gantt);
 }
 
 // 最后将插件导出，并在main.js中通过Vue.use()即可使用插件
-export default myPlugin;
+export default gantt;
+// export const vGanttChart = gantt;
